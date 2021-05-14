3ed242bc63900fa31ffd069df817e3e06d48f6b1
mnisicyprian <mnisicyprian@gmail.com>
2021-05-14 14:45:43 +0200

    first commit

---
+++ src/apdv400e/APDV400E.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package apdv400e;
+
+/**
+ *
+ * @author 201811030
+ */
+public class APDV400E {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+  int a = 6;
+        int b = 7;
+        
+        int c = a + b;
+        
+        System.out.println("a=6 + b=7 is equal to"+c);
+                
+    }
+    
+}
